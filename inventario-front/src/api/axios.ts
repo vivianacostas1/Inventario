dd9284caad255f5b343282b95caf2d2ef62b0772
vivianacostas1 <vialcore123@hotmail.com>
2026-08-25 13:32:52 -0400

    fix: usa variable de entorno para la api url en axios

--- inventario-front/src/api/axios.ts
+++ inventario-front/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Cambia 4000 por 3000
+ 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 });
 
 // Interceptor para agregar el token JWT automáticamente a cada petición
